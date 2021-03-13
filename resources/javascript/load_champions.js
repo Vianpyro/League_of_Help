dfef3f9fa769726e230367943fdee25c717bb70a
Vianpyro <10519369+Vianpyro@users.noreply.github.com>
2021-03-13 14:04:56 -0500

    mini update

--- resources/javascript/load_champions.js
+++ resources/javascript/load_champions.js
@@ -10,14 +10,22 @@ async function get_json_from_api(source_url) {
     // Get the language
     const languages = await get_json_from_api("https://ddragon.leagueoflegends.com/cdn/languages.json");
     const language = languages.includes(user_lang) ? user_lang: "en_US";
-    console.log(language);
 
     // Get the latest release
     const versions = await get_json_from_api("https://ddragon.leagueoflegends.com/api/versions.json");
-    console.log(`patch ${versions[0]}`);
+    
+    console.log(`patch ${versions[0]} - ${language}`);
 
     // Get the champions list
-    const champions = await get_json_from_api(`https://ddragon.leagueoflegends.com/cdn/${versions[0]}/data/${language}/champion.json`);
-    console.log(champions['data']);
-    document.getElementById('test').innerHTML = Object.keys(champions['data'])
+    const champions_data = await get_json_from_api(`https://ddragon.leagueoflegends.com/cdn/${versions[0]}/data/${language}/champion.json`);
+    const champions_list = Object.keys(champions_data['data'])
+    console.log(champions_data['data']);
+
+    for (champion of champions_list) {
+        document.getElementById('wrap').innerHTML += `
+        <figure><a>
+            <img class="champion_icon" src="https://ddragon.leagueoflegends.com/cdn/${versions[0]}/img/champion/${champion}.png" />
+            <figcaption>${champions_data['data'][champion]['name']}</figcaption>
+        </a></figure>`
+    };
 })();

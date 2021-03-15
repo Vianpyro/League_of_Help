56eba4a3c61bbcbb47d3eb6638b9275663c48509
Vianpyro <10519369+Vianpyro@users.noreply.github.com>
2021-03-14 20:26:42 -0400

    Added champions focus modal

--- resources/javascript/champions_modal.js
+++ resources/javascript/champions_modal.js
@@ -1,4 +1,16 @@
+// Display the modal
 async function open_modal(champion_data) {
     document.getElementById('champion_loading_screen_image').src = `${base_url}/cdn/img/champion/loading/${champion_data['id']}_0.jpg`
     document.getElementById('champion_loading_screen_name').innerHTML = champion_data['name']
+    document.getElementById('champion_loading_screen_title').innerHTML = champion_data['title']
+    
+    document.getElementById('modal_champion').style.display = 'flex';
+        
+    // Close modal by pressing esc key
+    document.body.onkeyup = e => {
+        if (e.key === 'Escape' || e.key === 'Esc') return document.getElementById('modal_champion').style.display = 'none';
+    }
+
+    // Close modal by clicking on modal-close
+    document.getElementById('modal_close').addEventListener('click', () => document.getElementById('modal_champion').style.display = 'none');
 }

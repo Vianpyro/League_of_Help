69398723a73cceaac56a9f4a8bf3c800b8daa342
Vianpyro <10519369+Vianpyro@users.noreply.github.com>
2021-04-08 00:37:07 -0400

    Added champions difficulty rating (by Riot)

--- resources/javascript/champions_modal.js
+++ resources/javascript/champions_modal.js
@@ -1,17 +1,25 @@
-// Display the modal
 async function open_modal(champion, patch, language) {
     const champion_data = await get_json_from_api(`${base_url}/cdn/${patch}/data/${language}/champion/${champion['id']}.json`)
 
+    // Splash art
     document.getElementById('champion_loading_screen_image').src = `${base_url}/cdn/img/champion/loading/${champion['id']}_0.jpg`
+
+    // Name and title
     document.getElementById('champion_loading_screen_name').innerHTML = champion['name']
     document.getElementById('champion_loading_screen_title').innerHTML = champion['title']
+
+    // Riot Difficulty
+    document.getElementById('riot_difficulty').title = `Riot Difficulty: ${champion['info']['difficulty']}/10`
+    document.getElementById('champion_riot_difficulty').value = champion['info']['difficulty']
     
+    // Spells images
     document.getElementById('champion_passive_image').src = `${base_url}/cdn/${patch}/img/passive/${champion_data['data'][champion['id']]['passive']['image']['full']}`
     document.getElementById('champion_q_spell_image').src = `${base_url}/cdn/${patch}/img/spell/${champion_data['data'][champion['id']]['spells'][0]['image']['full']}`
     document.getElementById('champion_w_spell_image').src = `${base_url}/cdn/${patch}/img/spell/${champion_data['data'][champion['id']]['spells'][1]['image']['full']}`
     document.getElementById('champion_e_spell_image').src = `${base_url}/cdn/${patch}/img/spell/${champion_data['data'][champion['id']]['spells'][2]['image']['full']}`
     document.getElementById('champion_r_spell_image').src = `${base_url}/cdn/${patch}/img/spell/${champion_data['data'][champion['id']]['spells'][3]['image']['full']}`
     
+    // Display the modal
     document.getElementById('modal_champion').style.display = 'flex';
         
     // Close modal by pressing esc key

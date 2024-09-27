8785b47520c6ffd51b63924d300a051d06db087a
Vianney <vianney@veremme.org>
2024-09-27 11:33:59 -0400

    Replace `'` with `"`

--- resources/javascript/main.js
+++ resources/javascript/main.js
@@ -5,18 +5,18 @@ async function get_json_from_api(source_url) {
 }
 
 const user_lang = navigator.language || navigator.userLanguage;
-const base_url = 'https://ddragon.leagueoflegends.com';
-const spell_keys = ['q', 'w', 'e', 'r'];
+const base_url = "https://ddragon.leagueoflegends.com";
+const spell_keys = ["q", "w", "e", "r"];
 const allEqual = arr => arr.every(val => val === arr[0]);
 
 (async function () {
-    const issues = await get_json_from_api('https://api.github.com/repos/Vianpyro/league_of_help/issues');
+    const issues = await get_json_from_api("https://api.github.com/repos/Vianpyro/league_of_help/issues");
     const tips = {};
 
     for (let element in issues) {
         if (issues[element].labels.length > 0) {
             for (let label of issues[element].labels) {
-                if (label.name == 'tip') {
+                if (label.name == "tip") {
                     tips[issues[element].title.substr(5)] = [issues[element].body, issues[element].html_url];
                 }
             }

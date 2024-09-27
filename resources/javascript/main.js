f93b94f70ef996d379ca840221ff0fdd425bf8bb
Vianney <vianney@veremme.org>
2024-09-27 11:44:40 -0400

    Add script to load champions dynamically

--- resources/javascript/main.js
+++ resources/javascript/main.js
@@ -1,3 +1,6 @@
-const userLanguage = navigator.language || navigator.userLanguage;
+const navigatorLanguage = navigator.language || navigator.userLanguage;
 const dataDragonUrl = "https://ddragon.leagueoflegends.com";
 const spellKeys = ["q", "w", "e", "r"];
+
+// Verify if the user is using a language supported by Data Dragon
+const supportedLanguages = [null];

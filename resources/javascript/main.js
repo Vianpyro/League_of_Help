66e2d87efa5bba4b753acb4d894f271b51237fe3
Vianney <vianney@veremme.org>
2024-09-27 13:17:04 -0400

    Refactor language selection logic in main.js

--- resources/javascript/main.js
+++ resources/javascript/main.js
@@ -1,6 +1,20 @@
-const navigatorLanguage = navigator.language || navigator.userLanguage;
 const dataDragonUrl = "https://ddragon.leagueoflegends.com";
 const spellKeys = ["q", "w", "e", "r"];
 
-// Verify if the user is using a language supported by Data Dragon
-const supportedLanguages = [null];
+async function getGameVersions() {
+    const response = await fetch(`${dataDragonUrl}/api/versions.json`);
+    return response.json();
+}
+
+async function getGameLanguages() {
+    const response = await fetch(`${dataDragonUrl}/cdn/languages.json`);
+    return response.json();
+}
+
+(async () => {
+    const languages = await getGameLanguages();
+    const navigatorLanguage = navigator.language || navigator.userLanguage;
+    const language = languages.includes(navigatorLanguage) ? navigatorLanguage : "en_US";
+
+    console.log(language);
+})();
